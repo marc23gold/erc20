f138f5f84c0baf51aed58ba02ac9877f89cdf5ea
Marcx <haitianhero6@gmail.com>
2024-04-30 18:49:41 -0400

    tests

---
+++ test/TestERC20.t.sol
@@ -0,0 +1,18 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Test} from "forge-std/Test.sol";
+import {OPToken} from "../src/OPToken.sol";
+import {DeployERC20} from "../script/DeployERC20.s.sol";
+
+contract TokenTest is Test {
+
+    
+    OurToken public ot;
+    DeployERC20 public deployer;
+    function setUp() public {
+        deployer = new DeployERC20();
+        ourToken = deployer.run();
+    }
+}
