5414320c5e2e26ad08f964c0066c2d5954df0444
Marcx <haitianhero6@gmail.com>
2024-04-30 19:30:36 -0400

    tests

--- test/TestERC20.t.sol
+++ test/TestERC20.t.sol
@@ -11,8 +11,37 @@ contract TokenTest is Test {
 
     OPToken public ot;
     DeployERC20 public deployer;
+
+    address bob = makeAddr("bob");
+    address alice = makeAddr("alice");
+
+    uint256 public constant STARTBALANCE = 10 ether;
+
     function setUp() public {
         deployer = new DeployERC20();
         ot = deployer.run();
+
+        vm.prank(msg.sender);
+        ot.transfer(bob, STARTBALANCE);
     }
+
+    function testBobBalance() public {
+        assertEq(ot.balanceOf(bob), STARTBALANCE);
+    }
+
+    function testAllowancesWork() public {
+        uint256 initalAllowance = 5; 
+        //bob approves alice to spend tokens on his behalf 
+        vm.prank(bob);
+        ot.approve(alice, initalAllowance);
+
+        uint256 transferAmount = 3;
+
+        vm.prank(alice);
+        ot.transferFrom(bob,alice, transferAmount);
+
+        assertEq(ot.balanceOf(alice), transferAmount);
+        assertEq(ot.balanceOf(bob), STARTBALANCE - transferAmount);
+
+    }  
 }
